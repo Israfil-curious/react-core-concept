ecc77f2b69aa560d72b8de36d08b05052e20b526
Israfil-curious <67516436+Israfil-curious@users.noreply.github.com>
2020-12-13 00:00:37 +0600

    creating component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit done hey! <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Person></Person>
+        <Person></Person>
+        <Person></Person>
       </header>
     </div>
   );
+
+  function Person() {
+    return (
+      <div style={{border: "2px solid orange", padding: "10px", margin: "20px"}}>
+        <h1>Hello World!</h1>
+        <h2>I'm a programmer</h2>
+      </div>
+    )
+  }
 }
 
 export default App;
